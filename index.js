f2cad8e64fddbdbf8fad877819260501dcf3fc5e
mkamar <mohamad.kamar@agoda.com>
2024-06-01 03:47:17 +0700

    Add basic html server

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
+const { createServer } = require("node:http");
+const fs = require("fs");
 
-const { createServer } = require('node:http');
-
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 
 const port = 3000;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+
+  fs.readFile("./index.html", function (err, html) {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Error");
+      return;
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(html);
+    res.end();
+  });
 });
 
 server.listen(port, hostname, () => {

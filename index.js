89539d230bb773dafbe702a6afe7d80436687f1a
mkamar <mohamad.kamar@agoda.com>
2024-06-01 10:34:35 +0700

    Add file checking

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const { createServer } = require("node:http");
 const fs = require("fs");
+const path = require('node:path'); 
 
 const hostname = "127.0.0.1";
 
 const port = 3000;
+
+
+const isFile = fileName => fs.lstatSync(fileName).isFile();
 
 const server = createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -20,16 +24,43 @@ const server = createServer((req, res) => {
     return;
   }
 
-  fs.readFile("./index.html", function (err, html) {
-    if (err) {
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Internal Server Error");
+  const requestPath = req.url;
+  if (requestPath === "/") {
+    fs.readFile("./index.html", function (err, html) {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(html);
+      res.end();
+    });
+    return;
+  }
+  else {
+    if(isFile(requestPath)){
+      fs.readdirSync(folderPath)
+        .map(fileName => {
+          return path.join(folderPath, fileName);
+        })
+        .filter(isFile);
       return;
     }
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(html);
-    res.end();
-  });
+
+    fs.readFile(`./${requestPath}`, function (err, html) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Path not found");
+        return;
+      }
+      
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(html);
+      res.end();
+    });
+  }
+
 });
 
 server.listen(port, hostname, () => {
